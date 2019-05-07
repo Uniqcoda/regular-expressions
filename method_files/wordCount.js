3dddfa8b3555dfe4e4266af3fee6e2d4bd29b633
Mary Etokwudo <etokwudomary@gmail.com>
2019-05-07 16:42:35 +0100

    pass test for the wordCount method

--- method_files/wordCount.js
+++ method_files/wordCount.js
@@ -1,2 +1,8 @@
 
+let wordsArray = require('../method_files/wordsArray');
+String.prototype.wordCount = function () {
+  let count = this.wordsArray().length;
+  return count;
+}
+
 module.exports = String.prototype.wordCount;
