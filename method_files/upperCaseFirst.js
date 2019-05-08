90b5535e009ed50aa8c21df3e1c43cde93abe01a
Mary Etokwudo <etokwudomary@gmail.com>
2019-05-08 15:51:00 +0100

    pass test for the upperCaseFirst method

--- method_files/upperCaseFirst.js
+++ method_files/upperCaseFirst.js
@@ -1,7 +1,7 @@
 let toUpper = require('../method_files/toUpper');
 
 String.prototype.upperCaseFirst = function () {   
-
+   return (this.charAt(0).toUpper() + this.slice(1));
 
 }
 module.exports = String.prototype.upperCaseFirst;
