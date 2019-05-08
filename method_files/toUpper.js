178887c32074033e4d1e7429de54d4f6ed544a50
Mary Etokwudo <etokwudomary@gmail.com>
2019-05-08 13:17:20 +0100

    create the character converter method

--- method_files/toUpper.js
+++ method_files/toUpper.js
@@ -1,2 +1,8 @@
 // a method that converts every lower case character in a string to uppercase
+
+String.prototype.toUpper = function () {
+  let regex = /([a-z])/g;
+
+}
+
 module.exports = String.prototype.toUpper;
