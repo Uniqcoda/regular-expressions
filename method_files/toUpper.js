7c16b1e9dace9085b907af71bae6b0978706c3f9
Mary Etokwudo <etokwudomary@gmail.com>
2019-05-08 13:19:02 +0100

    pass test for the toUpper method

--- method_files/toUpper.js
+++ method_files/toUpper.js
@@ -1,8 +1,11 @@
 // a method that converts every lower case character in a string to uppercase
+// import the method for converting characters
+let convertCharacter = require('../method_files/convertCharacter');
 
 String.prototype.toUpper = function () {
   let regex = /([a-z])/g;
-
+  // find the character the matches the regex and replace it with its uppercase equivalent
+  return this.replace(regex, convertCharacter);
 }
 
 module.exports = String.prototype.toUpper;
