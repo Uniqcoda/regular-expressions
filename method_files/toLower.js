0e7759fe880299b4aabc7561ff6402dc725ec199
Mary Etokwudo <etokwudomary@gmail.com>
2019-05-08 13:30:05 +0100

    pass test for the toLower method

--- method_files/toLower.js
+++ method_files/toLower.js
@@ -1,2 +1,11 @@
+// a method that converts every upper case character in a string to lowercase
+// import the method for converting characters
+let convertCharacter = require('../method_files/convertCharacter');
 
-// module.exports = toLower;
+String.prototype.toLower = function () {
+  let regex = /([A-Z])/g;
+  // find the character the matches the regex and replace it with its lowercase equivalent
+  return this.replace(regex, convertCharacter);
+}
+
+module.exports = String.prototype.toLower;
